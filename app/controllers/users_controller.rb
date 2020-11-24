dfbc577dcfd6f7aa3b66e02d28c95ee37e5404a3
Agata Filus <agatafilus@gmail.com>
2020-11-24 15:13:16 +0000

    Added edit form on user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @experiences = Experience.where(host_id: current_user.id)
     @bookings = Booking.where(customer_id: current_user.id)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(strong_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:user).permit(:name, :bio, photos:[])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :bookings, only: [:destroy]
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
 
 
