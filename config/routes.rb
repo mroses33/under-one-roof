a441843b64c26e2571433581007dadf14a8668d8
Mollie Semmes <mollie.rose279@gmail.com>
2020-11-24 14:20:43 +0000

    added supply list

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :experiences do
+    resources :items, except: :show
     resources :bookings, only: [:new, :create]
   end
 
