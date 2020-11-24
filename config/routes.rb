926cfe564f3b724f23e69bb334a14177eb65b858
Mollie Semmes <mollie.rose279@gmail.com>
2020-11-24 11:33:44 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :experiences do
-    resources :bookings, only: [:new, :create, :show, :edit, :update]
+    resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:destroy]
+  resources :bookings, only: [:destroy, :show, :edit, :update]
 end
