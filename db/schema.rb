9b26f338bd10b0864b65476da83c2aeeb0569a53
Mollie Semmes <mollie.rose279@gmail.com>
2020-12-01 12:38:09 +0000

    fixed modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_122542) do
+ActiveRecord::Schema.define(version: 2020_12_01_104500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_122542) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "duration"
+    t.string "tagline"
     t.index ["host_id"], name: "index_experiences_on_host_id"
   end
 
